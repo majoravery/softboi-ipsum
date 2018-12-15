0e5b81cad22c993f6bb64da9e0fee288fb02490b
coolsteed <vivianaverylim@gmail.com>
2018-12-15 23:02:55 +0100

    Updates

---
+++ precache-manifest.4318f71655154f09493846b8ef39b76f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c999066f0164c4898f70",
+    "url": "/softboi-ipsum/static/js/runtime~main.c999066f.js"
+  },
+  {
+    "revision": "13d7071d165d98e04467",
+    "url": "/softboi-ipsum/static/js/main.13d7071d.chunk.js"
+  },
+  {
+    "revision": "f441e4f58c8c60719dbc",
+    "url": "/softboi-ipsum/static/js/1.f441e4f5.chunk.js"
+  },
+  {
+    "revision": "13d7071d165d98e04467",
+    "url": "/softboi-ipsum/static/css/main.18137ffc.chunk.css"
+  },
+  {
+    "revision": "ec06732ae5d10b71e9a2b9b5266c6352",
+    "url": "/softboi-ipsum/index.html"
+  }
+];
